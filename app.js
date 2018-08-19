1383c11482df03b873f20811e3ec3fd667cf417e
izzuddin ahsanujunda <izzuddin.ahsanujunda@gmail.com>
2018-08-20 03:46:35 +0700

    error page

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    site: 'Mira and Junda'
+  });
 });
 
 module.exports = app;
