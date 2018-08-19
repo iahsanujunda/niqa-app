233da5f915f08c02a0968e547336e07bc31c1970
izzuddin ahsanujunda <izzuddin.ahsanujunda@gmail.com>
2018-08-19 20:57:30 +0700

    content.json ready

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const content = require('./../public/text/content.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Mira and Junda',
-    site: 'Mira and Junda Wedding'
+    site: 'Mira and Junda Wedding',
+    eats: content[1].eat,
+    souvenirs: content[4].souvenir
   });
 });
 
+console.log(content[1].eat);
 
-/* GET home page. */
+
+/* GET sample page. */
 router.get('/sample', function(req, res, next) {
   res.render('sample', { title: 'Express' });
 });
