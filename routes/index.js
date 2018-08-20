3ca2e3e9d2a3c18753fc26f2b40662e202c565e2
iahsanujunda <the.zlood@gmail.com>
2018-08-21 00:24:31 +0700

    move content.js from public

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-const content = require('./../public/text/content.json');
+const content = require('./../content/content.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
